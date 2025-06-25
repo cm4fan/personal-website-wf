4d255987eacb992b9091866eed3650ba4763511a
Nikolay <nguryanov@ptsecurity.com>
2025-06-26 04:11:04 +0700

    WIP CSS changes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 // Смена темы
 
-const lightStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=light]');
-const darkStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]');
-const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)');
 const switcherRadios = document.querySelectorAll('.switcher__radio');
 
 function setupSwitcher() {
     const savedScheme = getSavedScheme();
-
     if (savedScheme !== null) {
         const currentRadio = document.querySelector(`.switcher__radio[value=${savedScheme}]`);
-        currentRadio.checked = true;
+        if (currentRadio) currentRadio.checked = true;
     }
-
     [...switcherRadios].forEach((radio) => {
         radio.addEventListener('change', (event) => {
             setScheme(event.target.value);
@@ -20,19 +15,7 @@ function setupSwitcher() {
     });
 }
 
-function setupScheme() {
-    const savedScheme = getSavedScheme();
-    const systemScheme = getSystemScheme();
-
-    if (savedScheme === null) return;
-
-    if (savedScheme !== systemScheme) {
-        setScheme(savedScheme);
-    }
-}
-
 function setScheme(scheme) {
-    // Only set data-theme attribute for inline variables
     if (scheme === 'auto') {
         document.documentElement.removeAttribute('data-theme');
         clearScheme();
@@ -40,33 +23,6 @@ function setScheme(scheme) {
         document.documentElement.setAttribute('data-theme', scheme);
         saveScheme(scheme);
     }
-}
-
-function switchMedia(scheme) {
-    let lightMedia;
-    let darkMedia;
-
-    if (scheme === 'auto') {
-        lightMedia = '(prefers-color-scheme: light)';
-        darkMedia = '(prefers-color-scheme: dark)';
-    } else {
-        lightMedia = (scheme === 'light') ? 'all' : 'not all';
-        darkMedia = (scheme === 'dark') ? 'all' : 'not all';
-    }
-
-    [...lightStyles].forEach((link) => {
-        link.media = lightMedia;
-    });
-
-    [...darkStyles].forEach((link) => {
-        link.media = darkMedia;
-    });
-}
-
-function getSystemScheme() {
-    const darkScheme = darkSchemeMedia.matches;
-
-    return darkScheme ? 'dark' : 'light';
 }
 
 function getSavedScheme() {
@@ -82,8 +38,6 @@ function clearScheme() {
 }
 
 setupSwitcher();
-setupScheme();
-
 
 // Автозапуск видео
 
